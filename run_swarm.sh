bd00db3bd0df02a506472aa7d42a9c8d8b3a1453
mapreducedog <matsperk@gmail.com>
2017-09-29 20:12:10 +0200

    added instructions to run_swarm.sh

--- run_swarm.sh
+++ run_swarm.sh
@@ -1,1 +1,6 @@
-anapy swarmass.py
+#!/bin/bash
+#replace anapy with your anaconda python executable
+#for example, if you have anaconda python at the default ~/anaconda3/bin/python
+#the line below should be : ~/anaconda3/bin/python swarmass.py "$@"
+
+anapy swarmass.py "$@"
