f57187f94acc6eefff8ad11986b8800da349bbc6
mapreducedog <matsperk@gmail.com>
2017-09-30 13:01:38 +0200

    use this to view interactively later

---
+++ view_sequence.py
@@ -0,0 +1,22 @@
+#builtin
+import sys
+import json
+
+#own
+import check_score
+
+def view_sequence(filename):
+    with open(filename, 'r') as infile:
+        seqdic = json.load(infile)
+    check_score.test_plot(seqdic['move_sequence'],seqdic['polar_sequence'])
+check_score.show_plots = True
+check_score.save_plots = False
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or sys.argv[-1] in {'-h', '--help'}:
+        print("usage: python view_sequence <sequence_file(s)>")
+    else:
+        
+        for filename in sys.argv[1:]:
+            view_sequence(filename)
