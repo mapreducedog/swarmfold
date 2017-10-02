c68b413f304668583f174f2944ce915ada403bde
mapreducedog <matsperk@gmail.com>
2017-10-02 14:21:58 +0200

    Now reads from external coordinates in json

--- view_sequence.py
+++ view_sequence.py
@@ -2,13 +2,16 @@
 import sys
 import json
 
+#external
+import numpy as np
 #own
 import check_score
 
 def view_sequence(filename):
     with open(filename, 'r') as infile:
         seqdic = json.load(infile)
-    check_score.test_plot(seqdic['move_sequence'],seqdic['polar_sequence'])
+    coord_sequence = [np.matrix(xyz) for xyz in seqdic['coord_sequence']]
+    check_score.make_plot(coord_sequence, seqdic['polar_sequence'], show_plot = True,save_plot = False, dimensionality=seqdic['dimensionality'])
     
     
 
@@ -17,6 +20,5 @@ if __name__ == '__main__':
     if len(sys.argv) == 1 or sys.argv[-1] in {'-h', '--help'}:
         print("usage: python view_sequence <sequence_file(s)>")
     else:
-        
         for filename in sys.argv[1:]:
             view_sequence(filename)
