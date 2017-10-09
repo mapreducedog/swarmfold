5d745ea4b1e5ec5a09804d335f7774fc7de191d7
mapreducedog <matsperk@gmail.com>
2017-10-09 17:22:10 +0200

    Now takes folders as arguments, and views chronologically

--- view_sequence.py
+++ view_sequence.py
@@ -1,6 +1,12 @@
+'''
+
+'''
+
 #builtin
 import sys
 import json
+import os
+import glob
 
 #external
 import numpy as np
@@ -8,17 +14,29 @@ import numpy as np
 import check_score
 
 def view_sequence(filename):
+    print(filename)
     with open(filename, 'r') as infile:
         seqdic = json.load(infile)
     coord_sequence = [np.matrix(xyz) for xyz in seqdic['coord_sequence']]
     check_score.make_plot(coord_sequence, seqdic['polar_sequence'], show_plot = True,save_plot = False, dimensionality=seqdic['dimensionality'])
     
     
-
+def view_sequence_from_folder(foldername):
+    files = glob.glob(os.path.join(foldername, "*.json"))
+    files.sort(key = os.path.getctime)
+    print(files)
+    for filename in files:
+        view_sequence(filename)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1 or sys.argv[-1] in {'-h', '--help'}:
-        print("usage: python view_sequence <sequence_file(s)>")
+        print(
+        '''usage: python view_sequence.py <sequence_file>...
+    python view_sequence.py <folder/with/sequence/files>...
+    ''')
     else:
         for filename in sys.argv[1:]:
-            view_sequence(filename)
+            if os.path.isdir(filename):
+                view_sequence_from_folder(filename)
+            else:
+               view_sequence(filename)
